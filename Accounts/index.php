d6d553412bc4059783aa0b462ac35d061bff198a
Bohdan <bogmira777@gmail.com>
2022-09-25 14:03:00 +0200

    First commit

---
+++ Accounts/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$users1 = ["John" => "qwerty", "Nicole" => "asdf", "Mark" => "ww"];
+$users2 = ["Joan" => "1234", "Mark" => "poiu", "Nicole" => "ggg"];
+
+print_r(array_merge($users1, $users2));
